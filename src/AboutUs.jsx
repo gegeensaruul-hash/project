b8b1cc5219bce82b9d972da37e555d3975adc2a6
gegeensaruul <gegeensaruul@gmail.com>
2026-06-03 16:06:41 +0800

    Add all components

---
+++ src/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>Welcome to Paradise Nursery! We are passionate about bringing nature into your home. Founded in 2024, we offer a wide selection of beautiful houseplants.</p>
+      <p>Our mission is to make plant ownership accessible and enjoyable for everyone.</p>
+    </div>
+  );
+}
+export default AboutUs;
